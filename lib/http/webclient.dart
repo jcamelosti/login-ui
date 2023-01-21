43b2967bf6023229bdf3f5e9fbdf6a2fcac1aef8
Josué Camelo <cloudone@josuecamelo.com>
2023-01-21 12:35:42 -0300

    Http Request Lib

---
+++ lib/http/webclient.dart
@@ -0,0 +1,11 @@
+import 'package:http/http.dart';
+import 'package:http_interceptor/http/intercepted_client.dart';
+import 'package:login_ui/http/interceptors/logging_interceptor.dart';
+
+final Client client = InterceptedClient.build(
+  interceptors: [LoggingInterceptor()],
+  requestTimeout: const Duration(seconds: 5),
+);
+
+const String baseUrl = 'https://odontoapi.josuecamelo.com/';
+
