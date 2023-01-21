a1cf1d60a0a386b252edc44bcb617c00e0ae1f61
Josué Camelo <cloudone@josuecamelo.com>
2023-01-21 11:34:03 -0300

    Botões

--- lib/login_demo.dart
+++ lib/login_demo.dart
@@ -13,7 +13,7 @@ class _LoginDemoState extends State<LoginDemo> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Login Page"),
+        title: const Text("Login Page"),
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -49,6 +49,25 @@ class _LoginDemoState extends State<LoginDemo> {
                     hintText: 'Enter secure password'),
               ),
             ),
+            TextButton(
+              child: const Text('Forgot Password',  style: TextStyle(color: Colors.blue, fontSize: 15),),
+              onPressed: () {},
+            ),
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: ElevatedButton.icon(
+                icon: const Icon(Icons.login, size: 18),
+                label: const Text('Login'),
+                onPressed: () {},
+              ),
+            ),
+            const SizedBox(
+              height: 130,
+            ),
+            const Text('New User? Create Account')
           ],
         ),
       )
